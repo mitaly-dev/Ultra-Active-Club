50c6535fb0eb1abc8e4d7cadafb1407e4b3fae2b
mitaly-dev <mitalyy.dev@gmail.com>
2022-09-28 11:01:52 -0700

    create profile component and add a break section

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { act } from 'react-dom/test-utils';
 
-const Activity = ({activity}) => {
+const Activity = ({activity,addToList}) => {
+let {age,description,picture,time,title}=activity
+
     return (
-        <div className="card w-full bg-white shadow-xl rounded-lg">
-        <figure className='p-3'><img src="https://placeimg.com/400/225/arch" alt="Shoes" className='rounded-lg'/></figure>
-        <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="card w-full bg-white shadow-xl rounded-lg border">
+        <figure className='p-3'><img src="https://placeimg.com/400/225/arch" alt="Shoes" className='rounded-xl'/></figure>
+        <div className="card-body py-0 px-4">
+            <h2 className="card-title">{title}</h2>
+            <p>{description.length>50?description.slice(0,50):description}</p>
+            <p>Time Required: {time} </p>
             <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+            <button onClick={()=>addToList(activity.time)} className="w-full rounded-lg py-2 text-white bg-[#5D5FEF] border-none ">Add to list</button>
             </div>
         </div>
         </div>
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,32 +1,60 @@
 import React, { useEffect, useState } from 'react';
 import Activity from '../Activity/Activity';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee, faHandHoldingHeart, faHandsHoldingChild } from '@fortawesome/free-solid-svg-icons'
+import { faHandHoldingHeart} from '@fortawesome/free-solid-svg-icons'
+import Profile from '../Profile/Profile';
+import Details from '../Details/Details';
+
+
 
 const Main = () => {
     let [activities,setActivities] = useState([])
+    let [addActivity,setAddActivity]=useState([])
+    let [addBreak,setAddBreak]=useState(0)
 
     useEffect(()=>{
         fetch('activities.json')
         .then(res=>res.json())
         .then(data=>setActivities(data))
     },[])
+
+    const addToList=(activity)=>{
+        setAddActivity([...addActivity,activity])
+    }
+
+    const handleBreak=(event)=>{
+        setAddBreak(event.target.innerText)
+    }
+
+ 
     return (
         <div className='grid grid-cols-8'>
-            <div className='col-span-6 px-20 py-20 bg-[#F2F4FA]'>
+            <div className='col-span-6 px-16 py-20 bg-[#F2F4FA]'>
                 <div className='flex text-2xl text-[#5D5FEF] pb-6'>
                     <FontAwesomeIcon icon={faHandHoldingHeart}></FontAwesomeIcon>
                 <h3 className='pl-3 uppercase font-medium'>ultra-active-club</h3>
                 </div>
-                <p>Select today's tree planting</p>
-             <div className='grid grid-cols-3 gap-6'>
+                <p className='text-xl font-medium pb-6'>Select today's tree planting</p>
+             <div className='grid grid-cols-3 gap-5'>
              {
-                activities.map(activity=><Activity activity={activity}></Activity>)
+                activities.map(activity=><Activity activity={activity} key={activity.id} addToList={addToList}></Activity>)
              }
              </div>
             </div>
-            <div className='col-span-2 bg-slate-500'>
-                <h3>Aside</h3>
+            <div className='col-span-2'>
+               <div className=' bg-white px-6 py-10 sticky top-0'>
+               <Profile></Profile>
+               <h3 className='font-semibold text-xl'>Add A Break</h3>
+                <div onClick={(e)=>handleBreak(e)} className='flex justify-between bg-[#F2F4FA] px-5 py-3 rounded-lg my-5 font-semibold'>
+                   <button className='bg-white rounded-full p-3 py-2'>10</button>
+                   <button className='bg-white rounded-full p-3 py-2'>20</button>
+                   <button className='bg-white rounded-full p-3 py-2'>30</button>
+                   <button className='bg-white rounded-full p-3 py-2'>40</button>
+                   <button className='bg-white rounded-full p-3 py-2'>50</button>
+                </div>
+                <h3 className='font-semibold text-xl'>Tree Planting Details</h3>
+                <Details></Details>
+               </div>
             </div>
         </div>
     );
