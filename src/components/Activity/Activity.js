97054e5a6111ab965acf8ec701860f1fa18da42d
mitaly-dev <mitalyy.dev@gmail.com>
2022-09-28 13:21:41 -0700

    all activity is loaded in display

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { act } from 'react-dom/test-utils';
 
 const Activity = ({activity,addToList}) => {
-let {age,description,picture,time,title}=activity
+let {category,picture,time,title}=activity
+console.log(picture)
 
     return (
         <div className="card w-full bg-white shadow-xl rounded-lg border">
-        <figure className='p-3'><img src="https://placeimg.com/400/225/arch" alt="Shoes" className='rounded-xl'/></figure>
+        <figure className='p-3 h-[220px]'><img src={`${picture}`} alt="Shoes" className='rounded-xl h-full object-cover'/></figure>
         <div className="card-body py-0 px-4">
             <h2 className="card-title">{title}</h2>
-            <p>{description.length>50?description.slice(0,50):description}</p>
+            <p>Category: {category} </p>
             <p>Time Required: {time} Min</p>
             <div className="card-actions justify-end">
             <button onClick={()=>addToList(activity.time)} className="w-full rounded-lg py-2 text-white bg-[#5D5FEF] border-none ">Add to list</button>
